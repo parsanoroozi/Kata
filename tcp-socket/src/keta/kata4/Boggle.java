09ac47b5f6197567636f37804d4314b11fa94d3c
parsanoroozi <parsan6347@gmail.com>
2023-09-17 23:35:14 +0330

    adding Boggle class

---
+++ tcp-socket/src/keta/kata4/Boggle.java
@@ -0,0 +1,12 @@
+package keta.kata4;
+
+public class Boggle {
+    public Boggle(final char[][] board, final String word) {
+        // Your code here!
+    }
+
+    public boolean check() {
+        // Your code here too!
+        return false;
+    }
+}
